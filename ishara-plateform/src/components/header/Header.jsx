6534f7efab4b26d901aa1b92a9a3158515c4b654
ousafa <ouaamou.safa@gmail.com>
2025-03-12 16:31:34 +0000

    Set up the development environment to start working

---
+++ ishara-plateform/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+function Header() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Hello Ishara </h1>
+      <button className="btn btn-primary">Click Me</button>
+      <i class="bi bi-0-circle"></i>
+    </div>
+  );
+}
+export default Header;
