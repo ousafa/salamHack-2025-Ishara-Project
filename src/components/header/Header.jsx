262c5d85b640115154db7ff5eb9baa32746ae344
ousafa <ouaamou.safa@gmail.com>
2025-03-20 10:34:57 +0000

    Solving the problem of logo image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.css";
+import logo from "./isharaLogo.png";
 
 function Header({ newChatButton }) {
   return (
     <div className="d-flex align-items-center justify-content-center position-relative w-100 p-3">
       <img
-        src="/isharaLogo.png"
+         src={logo}
         alt="logo"
         className="logo position-absolute start-50 translate-middle-x "
         style={{ width: "110px" }}
