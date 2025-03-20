ebdb47f01c4d876ac031f5ddeb3a39e79c7af3c4
ousafa <ouaamou.safa@gmail.com>
2025-03-20 13:56:48 +0000

    version 0.0

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,10 @@ import cors from "cors";
 
 const app = express();
 const port = 5000;
-
-app.use(cors({
-  origin: 'https://ousafa.github.io/salamHack-2025-Ishara-Project/'
-}));
+app.use(cors());
 app.use(express.json());
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
-});
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Upload and extract text
 app.post("/upload", upload.single("file"), async (req, res) => {
@@ -26,7 +20,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     } = await Tesseract.recognize(req.file.buffer, "eng");
     res.json({ extractedText: text });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     res.status(500).send("Error processing image");
   }
 });
