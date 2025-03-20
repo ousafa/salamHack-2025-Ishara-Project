67e8d8a6039a20ee3a3a70c7cfed45630df2d44e
ousafa <ouaamou.safa@gmail.com>
2025-03-20 10:30:06 +0000

    version-0.0

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ function Header({ newChatButton }) {
   return (
     <div className="d-flex align-items-center justify-content-center position-relative w-100 p-3">
       <img
-        src="/public/isharaLogo.png"
+        src="/isharaLogo.png"
         alt="logo"
         className="logo position-absolute start-50 translate-middle-x "
         style={{ width: "110px" }}
